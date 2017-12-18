2dfea8fc580e846820118d9729c38371576c7453
Kate Dobrilko <ekaterina.dobrilko@exposit.com>
2017-12-18 11:57:05 +0300

    Working with ui-router

--- youtube-test-app/client/app/app.module.js
+++ youtube-test-app/client/app/app.module.js
@@ -3,28 +3,19 @@ import 'normalize.css';
 import angular from 'angular';
 import appComponent from './app.component';
 import ComponentsModule from './components/components';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
+import { visualizer } from '@uirouter/visualizer';
 
 let myApp = angular.module('app', [
-    'ui.router',
+    uiRouter,
     ComponentsModule.name
 ]);
 
-myApp.config(function ($stateProvider) {
-    let helloState = {
-        name: 'hello',
-        url: '/hello',
-        template: './app.component.html'
-    };
-
-    let aboutState = {
-        name: 'about',
-        url: '/about',
-        template: '<style>.active { color: red; font-weight: bold; }</style><h3>Its the UI-Router hello world app!</h3>'
-    };
-
-    $stateProvider.state(helloState);
-    $stateProvider.state(aboutState);
-});
+myApp.config([
+    '$uiRouterProvider', $uiRouter => {
+        // Show the UI-Router Visualizer
+        visualizer($uiRouter);
+    }
+]);
 
 myApp.component('app', appComponent);
